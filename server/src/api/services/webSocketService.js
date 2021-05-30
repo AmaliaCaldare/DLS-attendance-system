d177c8476fd579b375a29f7642422b9f6412cf3e
Amalia Caldare <amaliacaldare@Amalias-MacBook-Pro.local>
2021-05-30 20:16:32 +0200

    implemented websocket on server side

---
+++ server/src/api/services/webSocketService.js
@@ -0,0 +1,57 @@
+import randomstring from 'randomstring';
+import socketIo from 'socket.io';
+
+const generateCode = () => {
+  const code = randomstring.generate(7);
+  return code;
+};
+
+const startTimer = (minutes, seconds) => {
+
+};
+
+let io;
+const connect = async (server) => {
+  io = socketIo(server);
+};
+
+const emitCode = async () => {
+  io.on(`connection`, (socket) => {
+    console.log(`Socket connected`);
+
+    const code = generateCode();
+    socket.emit(`code`, code);
+
+    let minutes = 1;
+    let seconds = 0;
+    let outOfTime = false;
+    const startCountdown = setInterval(() => {
+      if (seconds === 0) {
+        minutes -= 1;
+
+        seconds = 60;
+      }
+      if (minutes === 0) {
+        outOfTime = true;
+      }
+      seconds -= 1;
+      const time = { minutes, seconds };
+      if (seconds > 9) {
+        socket.emit(`countdown`, `0${time.minutes}:${time.seconds}`);
+      }
+      if (seconds <= 9) {
+        socket.emit(`countdown`, `0${time.minutes}:0${time.seconds}`);
+      }
+
+      if (outOfTime && seconds === 0) {
+        socket.emit(`countdown`, `00:00. Countdown finished.`);
+        clearInterval(startCountdown);
+      }
+    }, 1000);
+  });
+};
+
+export default {
+  connect,
+  emitCode
+};
--- server/src/service.js
+++ server/src/service.js
@@ -4,15 +4,18 @@ import "regenerator-runtime/runtime.js";
 import express from 'express';
 import config from 'config';
 import bodyparser from 'body-parser';
+import http from 'http';
 
 import routes from './api/routes';
 import mongodbService from './api/services/mongodbService';
+import webSocketService from "./api/services/webSocketService";
 
 let service;
 const start = async () => {
   await mongodbService.connect();
 
   const app = express();
+
   const port = config.get(`port`);
 
   app.use(bodyparser.json());
@@ -22,6 +25,20 @@ const start = async () => {
   routes(app);
 
   service = app.listen(port);
+  /* service = http.Server(app);
+  service.listen(port); */
+
+  app.get(`/`, (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+  });
+  // eslint-disable-next-line global-require
+  /* (const io = require(`socket.io`)(service);
+  io.on(`connection`, (socket) => {
+    console.log(`Connectd`);
+  });
+*/
+  await webSocketService.connect(service);
+  await webSocketService.emitCode();
   console.log(`Now listening to port`, port);
 };
 
