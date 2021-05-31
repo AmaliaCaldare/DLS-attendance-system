1178f03d4048a3b220c9922462dd81e95aa35a88
Amalia Caldare <amaliacaldare@Amalias-MacBook-Pro.local>
2021-05-31 13:54:46 +0200

    listens to on click

--- server/src/api/services/webSocketService.js
+++ server/src/api/services/webSocketService.js
@@ -15,35 +15,37 @@ const emitCode = async () => {
   io.on(`connection`, (socket) => {
     console.log(`Socket connected`);
 
-    const code = generateCode();
-    socket.emit(`code`, code);
+    socket.on(`clicked`, (data) => {
+      const code = generateCode();
+      socket.emit(`code`, code);
 
-    let minutes = 1;
-    let seconds = 0;
-    let outOfTime = false;
-    const startCountdown = setInterval(() => {
-      if (seconds === 0) {
-        minutes -= 1;
+      let minutes = 1;
+      let seconds = 0;
+      let outOfTime = false;
+      const startCountdown = setInterval(() => {
+        if (seconds === 0) {
+          minutes -= 1;
 
-        seconds = 60;
-      }
-      if (minutes === 0) {
-        outOfTime = true;
-      }
-      seconds -= 1;
-      const time = { minutes, seconds };
-      if (seconds > 9) {
-        socket.emit(`countdown`, `0${time.minutes}:${time.seconds}`);
-      }
-      if (seconds <= 9) {
-        socket.emit(`countdown`, `0${time.minutes}:0${time.seconds}`);
-      }
+          seconds = 60;
+        }
+        if (minutes === 0) {
+          outOfTime = true;
+        }
+        seconds -= 1;
+        const time = { minutes, seconds };
+        if (seconds > 9) {
+          socket.emit(`countdown`, `0${time.minutes}:${time.seconds}`);
+        }
+        if (seconds <= 9) {
+          socket.emit(`countdown`, `0${time.minutes}:0${time.seconds}`);
+        }
 
-      if (outOfTime && seconds === 0) {
-        socket.emit(`countdown`, `00:00. Countdown finished.`);
-        clearInterval(startCountdown);
-      }
-    }, 1000);
+        if (outOfTime && seconds === 0) {
+          socket.emit(`countdown`, `00:00. Countdown finished.`);
+          clearInterval(startCountdown);
+        }
+      }, 1000);
+    });
   });
 };
 
